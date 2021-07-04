682f774b37f5cd67016530538500dabbe21d9f83
yuanquan <yuanquan2011@qq.com>
2021-07-04 14:22:04 +0800

    Add 5. longestPalindrome

--- solved/5_Longest_Palindromic_Substring.cpp
+++ solved/5_Longest_Palindromic_Substring.cpp
@@ -4,7 +4,7 @@
  * @Email: yuanquan2011@qq.com
  * @Date: 2019-12-13 11:16:50
  * @LastEditors: yuanquan
- * @LastEditTime: 2020-01-20 16:08:36
+ * @LastEditTime: 2021-07-04 14:20:45
  * @Copyright: Copyright (c) 2019 Supcon
  *************************************************/
 #include "iostream"
@@ -26,31 +26,61 @@ using namespace std;
 string longestPalindrome(string s) {
 	int start = 0, end = 0, sLen = s.length();
 	if(sLen <= 1)return s;
-	for(int i=0; i < sLen; i++)
+	for(int i=0; i < sLen; ++i)
 	{
-		for(int endTmp=sLen-1; i <= endTmp; endTmp--)
+		// sub start: i-j
+		// sub end: j+i
+		int j = 1;
+		for(j=1; i-j>=0 && j+i<=sLen; ++j)
 		{
-			int endTTmp = endTmp;
-			for(int startTmp=i; startTmp < endTTmp; startTmp++, endTTmp--)
+			if(s[i-j] == s[j+i])
 			{
-				if(s.at(startTmp) != s.at(endTTmp)){break;}
-				else if(startTmp == endTTmp || (startTmp+1 == endTTmp) || startTmp+2==endTTmp)
+				if(2*j > end - start)
 				{
-					if(end-start < endTmp -i)
+					start = i - j;
+					end = i + j;
+				}
+			}
+			else
+			{
+				break;
+			}
+		}
+
+
+		// sub start: i-j
+		// sub end: i+j+1
+		if(i+1<sLen && s[i]==s[i+1])
+		{
+			if(1 > end - start)
+			{
+				start = i;
+				end = i + 1;
+			}
+			for(j=1; i-j>=0 && j+i+1<sLen; ++j)
+			{
+				if(s[i-j]==s[j+i+1])
+				{
+					if(2*j + 1 > end - start)
 					{
-						start = i;
-						end = endTmp;
+						start = i - j;
+						end = i + j + 1;
 					}
+				}
+				else
+				{
+					break;	
 				}
 			}
 		}
 	}
+
 	return s.substr(start, end-start+1);
 }
 
 int main(void)
 {
-	string s = "abacab";
+	string s = "ccc";
 	cout << longestPalindrome(s) << endl;
 	return 0;
 }
